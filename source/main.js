5992cdbbcdf3f227cd5095472f870b5c5b724186
platonovso <st.platonov@gmail.com>
2019-08-16 11:32:49 +0300

    stack

---
+++ source/main.js
@@ -0,0 +1,5 @@
+import './style.scss';
+
+/* Скрипты по умолчанию */
+import './components/_defaults/defaults';
+
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pug = require('./webpack_modules/pug');
+const extractCSS = require('./webpack_modules/css.extract');
+const css = require('./webpack_modules/css');
+const webpack = require('webpack');
+const sourceMap = require('./webpack_modules/sourceMap');
+const lintJS = require('./webpack_modules/js.lint');
+const lintCSS = require('./webpack_modules/sass.lint');
+const images = require('./webpack_modules/images');
+const babel = require('./webpack_modules/babel');
+const favicon = require('./webpack_modules/favicon');
+
+const PATHS = {
+  source: path.join(__dirname, 'source'),
+  build: path.join(__dirname, 'build'),
+};
+
+const config = merge([
+  {
+    entry: {
+      'main': PATHS.source + '/main.js',
+    },
+    output: {
+      path: PATHS.build,
+      filename: './js/[name].js',
+    },
+    watch: true,
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: PATHS.source + '/pages/index.pug',
+      }),
+    ],
+  },
+  pug(),
+  lintJS({paths: PATHS.sources}),
+  lintCSS(),
+  images(),
+  babel(),
+]);
+
+module.exports = function (env, argv) {
+  if (argv.mode === 'production') {
+    config.devtool = false;
+    config.watch = false;
+    return merge([
+      config,
+      extractCSS(),
+      favicon(),
+    ]);
+  }
+  if (argv.mode === 'development') {
+    return merge([
+      config,
+      extractCSS(),
+      sourceMap(),
+    ]);
+  }
+};
